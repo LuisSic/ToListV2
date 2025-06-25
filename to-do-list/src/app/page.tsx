18f3c46be2ff572520cb155f85acb842c5b5bbf8
Luis Sic <luis@relishiq.com>
2025-06-24 20:44:33 -0600

    public pages

--- to-do-list/src/app/page.tsx
+++ to-do-list/src/app/page.tsx
@@ -1,95 +1,173 @@
+import Add from "../../public/features/add-outline.svg";
+import Alert from "../../public/features/alert-circle-outline.svg";
+import Chatbox from "../../public/features/chatbox-outline.svg";
+import Checkmark from "../../public/features/checkmark-circle-outline.svg";
+import Flag from "../../public/features/flag-outline.svg";
+import Heart from "../../public/features/heart-outline.svg";
+import List from "../../public/features/list-outline.svg";
+import Notifications from "../../public/features/notifications-outline.svg";
+import Person from "../../public/features/person-add-outline.svg";
+import Refresh from "../../public/features/refresh-outline.svg";
+import Settings from "../../public/features/settings-outline.svg";
+import Chart from "../../public/features/stats-chart-outline.svg";
+import Photo1 from "../../public/features/photo-2.svg";
+
+import Linkedin from "../../public/features/linkedin.svg";
+import Microsoft from "../../public/features/microsoft.svg";
+import Tesla from "../../public/features/coca-cola.svg";
+import Xbox from "../../public/features/xbox-2.svg";
+import Facebook from "../../public/features/facebook-1.svg";
+import Netflix from "../../public/features/netflix-2015-logo.svg";
+import Youtube from "../../public/features/youtube.svg";
+import Sendgrid from "../../public/features/sendgrid-2.svg";
+import Button from "@/components/Button/Button";
 import Image from "next/image";
-import styles from "./page.module.css";
-
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Todo App</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface FeatureProps {
+  title: string;
+  text: string;
+  Svg: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 }
+
+const Feature = ({ title, text, Svg }: FeatureProps) => {
+  return (
+    <>
+      <div className="feature">
+        <Svg className="feature__icon" />
+        <h4 className="heading-4">{title}</h4>
+        <p className="feature__text">{text}</p>
+      </div>
+    </>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <div className="slogan">
+        <div className="slogan__text">
+          <h1 className="heading-1">
+            <span className="heading-1--main">Organize it all with To Do</span>
+          </h1>
+          <h2 className="heading-2">The all-in-one-to-do</h2>
+          <Button className="btn slogan-btn" text="Get Started" />
+        </div>
+        <Image
+          src={"/features/photo-1.svg"}
+          alt="Slogan Image"
+          width={1000}
+          height={500}
+        />
+      </div>
+      <section className="features">
+        <div className="features__header">
+          <Photo1 className="features__header--img" />
+          <h1 className="heading-1 features__header--center">
+            Features that people love
+          </h1>
+        </div>
+        <div className="feature__header">
+          <h3 className="heading-3">
+            Start each day feeling calm and in control
+          </h3>
+        </div>
+        <Feature
+          Svg={Add}
+          title="Quick Add"
+          text="lets you capture and organize tasks in seconds."
+        />
+        <Feature
+          Svg={Refresh}
+          title="Recurring due dates"
+          text="help you remember deadlines and build habits."
+        />
+        <Feature
+          Svg={Alert}
+          title="Task View"
+          text="gives you the complete picture of any task."
+        />
+
+        <div className="feature__header">
+          <h3 className="heading-3">Focus your energy on the right things</h3>
+        </div>
+        <Feature
+          Svg={Heart}
+          title="Favorites"
+          text="keep key projects, labels, or filters top of mind."
+        />
+        <Feature
+          Svg={List}
+          title="Sections & subtasks"
+          text="ensure that your projects stay neatly organized."
+        />
+        <Feature
+          Svg={Flag}
+          title="Priorities"
+          text="highlight the day’s most important tasks."
+        />
+        <div className="feature__header">
+          <h3 className="heading-3">Share the workload</h3>
+        </div>
+
+        <Feature
+          Svg={Person}
+          title="Delegate tasks"
+          text="to project collaborators and free up your mental space."
+        />
+        <Feature
+          Svg={Notifications}
+          title="Notifications"
+          text="let you know when people post comments, complete tasks, and more."
+        />
+        <Feature
+          Svg={Chatbox}
+          title="Task & project comments"
+          text="make essential information accessible to everyone."
+        />
+
+        <div className="feature__header">
+          <h3 className="heading-3">See tangible progress</h3>
+        </div>
+        <Feature
+          Svg={Settings}
+          title="Todoist Karma"
+          text="awards you points for completing tasks and maintaining your streaks."
+        />
+        <Feature
+          Svg={Chart}
+          title="Productivity visualizations"
+          text="display your daily & monthly progress in color-coded graphs."
+        />
+        <Feature
+          Svg={Checkmark}
+          title="Completed tasks"
+          text="help you keep track of all that you’ve accomplished in any given
+          project."
+        />
+      </section>
+      <section className="customers">
+        <p className="customers__description">
+          <span className="customers__description--main">
+            ToDo has helped millions of people complete
+          </span>
+          <span className="customers__description--sub">
+            over 1.5 billion tasks in 150+ million projects.
+          </span>
+        </p>
+
+        <Linkedin className="customers__item--1 customers__img" />
+        <Microsoft className="customers__item--2 customers__img" />
+        <Xbox className="customers__item--3 customers__img" />
+        <Netflix className="customers__item--4 customers__img" />
+
+        <Tesla className="customers__item--5 customers__img" />
+
+        <Facebook className="customers__item--6 customers__img" />
+        <Youtube className="customers__item--7 customers__img" />
+
+        <Sendgrid className="customers__item--8 customers__img" />
+      </section>
+    </>
+  );
+};
+
+export default Home;
--- to-do-list/src/app/team/page.tsx
+++ to-do-list/src/app/team/page.tsx
@@ -1,5 +1,85 @@
-function Team() {
-  return <div>Team</div>;
-}
+import CardProfile from "@/components/team/CardProfile";
+
+const teamMembers = [
+  {
+    mainTitle: "Geovanny Lopez",
+    subTitle: "Co-Founder and CEO",
+    photo: "/team/geo.jpeg",
+    linkProfile: "https://www.linkedin.com/in/geovanny-lopez/",
+    alt: "Geovanny",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus error, quis perferendis cum corrupti necessitatibus esse autem magnam provident unde nemo nihil repellendus quod aut porro velvero. Dolores, similique.",
+  },
+  {
+    mainTitle: "Luis Sic",
+    subTitle: "Co-Founder and CTO",
+    photo: "/team/luis.jpg",
+    linkProfile:
+      "https://www.linkedin.com/in/luis-antonio-sic-canil-868181116/",
+    alt: "Luis",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur accusantium, totam, ad reprehenderit eveniet error nihil, odio placeat ducimus aspernatur quos assumenda eos cupiditate ullam iusto sint. Doloremque, aspernatur quo.",
+  },
+  {
+    mainTitle: "Maria Reneé",
+    subTitle: "Full-Stack Engineering",
+    photo: "/team/renne.jpg",
+    linkProfile:
+      "https://www.linkedin.com/in/maria-rene%C3%A9-palma-07855416a/",
+    alt: "Maria Renee",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus error, quis perferendis cum corrupti necessitatibus esse autem magnam provident unde nemo nihil repellendus quod aut porro velvero. Dolores, similique.",
+  },
+  {
+    mainTitle: "Ricardo Hernandez",
+    subTitle: "Full-Stack Engineering",
+    photo: "/team/ricardo.jpg",
+    linkProfile:
+      "https://www.linkedin.com/in/ricardo-hernandez-ramirez-124824103/",
+    alt: "Ricardo",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus error, quis perferendis cum corrupti necessitatibus esse autem magnam provident unde nemo nihil repellendus quod aut porro velvero. Dolores, similique.",
+  },
+  {
+    mainTitle: "Jose Sic",
+    subTitle: "Quality Assurance Engineering",
+    photo: "/team/jose.jpg",
+    linkProfile:
+      "https://www.linkedin.com/in/jos%C3%A9-manuel-sic-canil-860b1b158/",
+    alt: "Jose",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus error, quis perferendis cum corrupti necessitatibus esse autem magnam provident unde nemo nihil repellendus quod aut porro velvero. Dolores, similique.",
+  },
+  {
+    mainTitle: "Julio Carpio",
+    subTitle: "Quality Assurance Engineering",
+    photo: "/team/carpio.jpg",
+    linkProfile: "https://www.linkedin.com/in/julio-carpio-89ba40a4/",
+    alt: "Carpio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus error, quis perferendis cum corrupti necessitatibus esse autem magnam provident unde nemo nihil repellendus quod aut porro velvero. Dolores, similique.",
+  },
+];
+const Team = () => {
+  const render = teamMembers.map((teamMember, index) => {
+    return (
+      <div className={`team__group group--${index}`} key={index}>
+        <CardProfile
+          mainTitle={teamMember.mainTitle}
+          subTitle={teamMember.subTitle}
+          description={teamMember.description}
+          photo={teamMember.photo}
+          linkProfile={teamMember.linkProfile}
+          alt={teamMember.alt}
+        />
+      </div>
+    );
+  });
+  return (
+    <>
+      <div className="team">{render}</div>
+    </>
+  );
+};
 
 export default Team;
